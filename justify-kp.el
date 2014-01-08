76848d07b917150ee788054584fe699be27b4d63
Matus Goljer <dota.keys@gmail.com>
2014-01-08 16:22:42 +0100

    Add support for different fonts in the same paragraph & properly handle multi-char glue

--- justify-kp.el
+++ justify-kp.el
@@ -108,6 +108,47 @@ If AT is non-nil and number, assume font at that buffer position."
   (let ((gstring (pj--get-gstring 0 (length string) (font-at (or at (point))) string)))
     (apply '+ (pj-mapcar 'lglyph-width gstring))))
 
+;; TODO: make this faster by somehow detecting the 'display space
+;; specs
+(defun pj--next-font-change (&optional p limit)
+  "Move point to next position where font changes.
+
+If optional argument P is a number, start from that position,
+defaults to current position.
+
+If optional argument LIMIT is a number, do not extend the search
+further than this position.  If no font change was found, the
+point will be at the LIMIT position or end of file (if this is
+smaller) after the function returns."
+  (setq p (or p (point)))
+  (setq limit (min (or limit (point-max)) (point-max)))
+  (goto-char p)
+  (flet ((get-next-font-name
+          ()
+          (let ((np (or (next-property-change p) limit)))
+            (if (>= np limit)
+                (setq p limit)
+              (elt (font-info (font-at (setq p (goto-char np)))) 0)))))
+    (let ((current-font (elt (font-info (font-at p)) 0)))
+      (while (equal current-font (get-next-font-name)))
+      (goto-char p))))
+
+(defun pj--get-line-data ()
+  (let ((limit (1- (cdr (bounds-of-thing-at-point 'line))))
+        (last-change (point))
+        next-change
+        gline
+        re)
+    (while (< (point) limit)
+      (setq next-change (pj--next-font-change (point) limit))
+      (setq gline (pj--get-gstring last-change next-change))
+      (setq last-change next-change)
+      (pj-mapc
+       (lambda (g)
+         (push (list :char (lglyph-char g) :width (lglyph-width g)) re))
+       gline))
+    (nreverse re)))
+
 (defun pj-get-space-width (gline)
   "Return width of space character using the font of GLINE.
 
@@ -117,79 +158,98 @@ This function assumes there is a space character in GLINE."
     (while (not (eq (aref header i) 32)) (cl-incf i))
     (lglyph-width (lgstring-glyph gline (1- i)))))
 
-;; TODO: right now the constant '5' is randomly chosen and doesn't
-;; corespond to font-width.  Find a generic way to get glyph widths
-;; (the gline approach is quite shitty)
-(defun pj--make-box (word total-width shrink stretch &optional glue)
+;; warning: word and whitespace come REVERSED!!!
+(defun pj--make-token (word whitespace total-width total-shrink total-stretch)
   "Make a box data structure out of WORD, which is a list of glyphs."
-  (let* ((width 0)
-         (chars (mapcar (lambda (c)
-                          (cl-incf width (lglyph-width c))
-                          (lglyph-char c))
-                        word))
-         (value (apply 'string chars)))
+  (let* ((word-width (--reduce-from (+ acc (pj-glyph-get it :width)) 0 word))
+         (white-width (--reduce-from (+ acc (pj-glyph-get it :width)) 0 whitespace))
+         (glue-width white-width)
+         (word-value (apply 'string (nreverse (--map (pj-glyph-get it :char) word))))
+         (white-value (apply 'string (--map (pj-glyph-get it :char) whitespace))))
     (when (and pj-hanging-punctuation
-               glue
-               (-any? (-cut s-ends-with-p <> value) pj-hanging-punctuation))
-      (cl-decf width 5)
-      (setq glue (plist-put glue :width
-                            (+ (plist-get glue :width) 5))))
+               (-any? (-cut s-ends-with-p <> word-value) (--map (car it) pj-hanging-punctuation)))
+      (-when-let* ((punct (substring word-value (1- (length word-value))))
+                   (punct-data (assoc punct pj-hanging-punctuation))
+                   (extension (floor (* (cadr punct-data) (pj-glyph-get (car word) :width)))))
+        (cl-incf glue-width extension)
+        (cl-decf word-width extension)))
     (list :type :box
-          :width width
-          :value value
-          :total-width (+ width total-width)
-          :total-shrink shrink
-          :total-stretch stretch
-          :glue glue)))
+          :width word-width
+          :value word-value
+          :total-width (+ word-width total-width)
+          :total-shrink total-shrink
+          :total-stretch total-stretch
+          :glue (when whitespace
+                  (list :type :glue
+                        :width glue-width
+                        :real-width white-width
+                        :shrink (/ white-width 3)
+                        :stretch (* white-width 2)
+                        :value white-value)))))
 
-;; TODO: handle the underflow boxes better (right now huge stretch
-;; fixes most of the issues)
-(defun pj--make-glue (width)
-  "Make a glue token with default WIDTH, shrinkability 1/3 of
-width and stretchability 1/2 of width."
-  (list :type :glue :width width :shrink (/ width 3) :stretch (/ (* width 6) 3) :value " "))
+
+(defmacro pj-glyph-get (glyph-data prop)
+  `(plist-get ,glyph-data ,prop))
 
 ;; this assumes one glue always follows one box -- I think any other
 ;; situation can be reduced to this anyway
 
-;; TODO: this needs to be reworked to take changes in text-properties
-;; into account.  Input should be a point in buffer: we take the line
-;; following the point, then walk over the words by face (font!!)
-;; property changes, take the glines of each segment, and produce the
-;; tokens.  At most one glue token can follow each non-glue
-;; token---this glue will be added as a property of the box, not as a
-;; separate item in the token stream.  Each glue will have to remember
-;; its apparent and real width (apparent width can extend over
-;; surrounding boxes, real width is what we stretch/shring when we
-;; actually render the glue)
-(defun pj-get-line-tokens (gline)
-  "Return a list of tokens for the current line.
+;; TODO: above function needs to be reworked to take changes in
+;; text-properties into account.  Input should be a point in buffer:
+;; we take the line following the point, then walk over the words by
+;; face (font!!)  property changes, take the glines of each segment,
+;; and produce the tokens.  At most one glue token can follow each
+;; non-glue token---this glue will be added as a property of the box,
+;; not as a separate item in the token stream.  Each glue will have to
+;; remember its apparent and real width (apparent width can extend
+;; over surrounding boxes, real width is what we stretch/shring when
+;; we actually render the glue)
+(defun pj--get-line-tokens ()
+  "Return a list of tokens on the current line.
 
-Each token is either \"box\" or \"glue\"---that is inter-word
-spacing.  Each token has a width and possible shrink and stretch
-values.  Boxes also has a value, the text they represent."
-  (let ((total-width 0)
-        (shrink 0)
-        (stretch 0)
-        (space-width (pj-get-space-width (copy-sequence gline)))
-        re
-        word)
-    (pj-mapc
-     (lambda (g)
-       (if (not (eq (lglyph-char g) 32))
-           (push g word)
-         (let* ((glue (pj--make-glue space-width))
-                (box (pj--make-box (nreverse word) total-width shrink stretch glue)))
-           (setq glue (plist-get box :glue))
-           (push box re)
-           (push glue re)
-           (setq word nil)
-           (cl-incf total-width (+ (plist-get box :width) (plist-get glue :width)))
-           (cl-incf shrink (plist-get glue :shrink))
-           (cl-incf stretch (plist-get glue :stretch)))))
-     gline)
-    (push (pj--make-box (nreverse word) total-width shrink stretch) re)
-    (nreverse re)))
+Each token has a width, a value (the text it represents) and an
+optional glue following it.  The glue has a width, a (usually
+whitespace) value, and a possible stretchability and
+shirnkability---when setting the line, glue represents the
+stretching or shrinking of the space between this token and the
+next.
+
+We must also distinguish an apparent width of the glue and the
+real width of the glue.  Apparent width can extend the glue into
+the surrounding tokens and is used in the break-point
+calculation.  Real width is the width of the whitespace this glue
+represent and is used when the line is actually rendered.  This
+distinction is necessary due to the rendering engine of Emacs."
+  (save-excursion
+    (flet ((add-token
+            (token)
+            (push token re)
+            (setq word nil)
+            (setq whitespace nil)
+            (cl-incf total-width (+ (plist-get token :width) (or (pj--plist-get token :glue :width) 0)))
+            (cl-incf total-shrink (or (pj--plist-get token :glue :shrink) 0))
+            (cl-incf total-stretch (or (pj--plist-get token :glue :stretch) 0))))
+      (let ((line-data (pj--get-line-data))
+            (total-width 0) (total-shrink 0) (total-stretch 0)
+            re
+            word whitespace)
+        (--each line-data
+          (cond
+           ;; read the word
+           ((and (not (eq (pj-glyph-get it :char) 32))
+                 (not whitespace))
+            (push it word))
+           ;; read the whitespace
+           ((and (eq (pj-glyph-get it :char) 32)
+                 word)
+            (push it whitespace))
+           ;; when we have both, we can process them into a token
+           (t
+            (let ((token (pj--make-token word whitespace total-width total-shrink total-stretch)))
+              (add-token token))
+            (push it word))))
+        (add-token (pj--make-token word whitespace total-width total-shrink total-stretch))
+        (nreverse re)))))
 
 (defun pj--break-badness (break-point)
   "Calculate badness of BREAK-POINT.
@@ -210,6 +270,7 @@ value of `pj--get-node-difference'."
   "Calculate demerits of line break."
   (expt (+ pj-demerits-line badness) 2))
 
+;; TODO: this assumes each node has a glue
 (defun pj--get-node-difference (node1 node2)
   "Get differences between NODE1 and NODE2."
   (list :distance (- (plist-get node2 :total-width)
@@ -294,37 +355,71 @@ This signifies that the active node should be disactivated."
                   (an (plist-get best :node))
                   (dem (plist-get best :demerits))
                   (line-width (plist-get best :distance)))
-             (setcdr cn (cons an (cons :distance
-                                       (cons line-width
-                                             (cons :demerits
-                                                   (cons dem (cons (car cn) (cdr cn))))))))
+             (setcdr cn (-cons* an
+                                :distance line-width
+                                :demerits dem
+                                :shrink-diff (plist-get best :shrink-diff)
+                                :stretch-diff (plist-get best :stretch-diff)
+                                (car cn) (cdr cn)))
              (setcar cn :parent)))))
-     ;; we can only consider boxes at this point
-     (--filter (eq :box (plist-get it :type)) tokens))
+     tokens)
     (let* ((x (-min-by (-on '> (lambda (x) (plist-get x :demerits))) active-nodes))
            (re (list x)))
       (while (setq x (plist-get x :parent))
         (push x re))
-      (cdr re))
-    ))
+      (cdr re))))
 
 (defun pj-breaklines (tokens)
   (save-excursion
-    (let* ((where (pj-justify tokens))
-           (where2 where))
-      (mapc
-       (lambda (ac)
-         (forward-char (+ (length (plist-get ac :value))
-                          (length (pj--plist-get ac :glue :value))))
-         (when where
-           (if (= (plist-get (car where) :total-width)
-                  (plist-get ac :total-width))
-               (progn
-                 (delete-char (- (length (pj--plist-get ac :glue :value))))
-                 (newline)
-                 (pop where)))))
-       (--filter (eq :box (plist-get it :type)) tokens))
-      where2)))
+    (flet ((recalc-adj-ratio
+            ()
+            (setq overflow 0)
+            (setq line-width (plist-get (car where) :distance))
+            (setq adjustment (- pj-line-width line-width))
+            (setq adj-ratio (cond
+                             ((<= adjustment 0)
+                              (/ (float adjustment) (plist-get (car where) :shrink-diff)))
+                             ((> adjustment 0)
+                              (/ (float adjustment) (plist-get (car where) :stretch-diff)))))))
+      (let* ((where (pj-justify tokens))
+             line-width adjustment adj-ratio
+             (overflow 0))
+        (when where
+          (recalc-adj-ratio)
+          (mapc
+           (lambda (ac)
+             ;; skip over the box
+             (forward-char (+ (length (plist-get ac :value))
+                              (or (length (pj--plist-get ac :glue :value)) 0)))
+             ;; modify the space display
+             (when where
+               (if (= (plist-get (car where) :total-width)
+                      (plist-get ac :total-width))
+                   (progn
+                     ;; TODO: text could also break between boxes with
+                     ;; no glue between them
+                     (put-text-property (- (point) (length (pj--plist-get ac :glue :value)))
+                                        (point) 'display "\n")
+                     (pop where)
+                     (when where (recalc-adj-ratio)))
+                 (when (plist-get ac :glue)
+                   (let* ((real-width (pj--plist-get ac :glue :real-width))
+                          (disp-width (+ real-width (* (if (<= adj-ratio 0)
+                                                           (pj--plist-get ac :glue :shrink)
+                                                         (pj--plist-get ac :glue :stretch)) adj-ratio)))
+                          (disp-width-whole (floor disp-width))
+                          (disp-width-decimal (- disp-width disp-width-whole))
+                          (current-width (if (progn
+                                               (cl-incf overflow disp-width-decimal)
+                                               (< overflow 1))
+                                             disp-width-whole
+                                           (cl-decf overflow 1)
+                                           (+ 1 disp-width-whole))))
+                     (put-text-property (- (point) (length (pj--plist-get ac :glue :value)))
+                                    (point)
+                                    'display `(space :width ,(list current-width))))))))
+           tokens)))))
+  t)
 
 (defun pj-breaklines-and-justify (tokens)
   (let ((lines (pj-breaklines tokens)))
@@ -381,12 +476,12 @@ This is the opposite of fill-paragraph."
   (interactive)
   (save-excursion
     (pj-unjustify-paragraph)
-    (pj-unfill-paragraph)
+    ;; (pj-unfill-paragraph)
     (let ((end (progn (forward-paragraph) (point)))
           (start (progn (backward-paragraph) (point))))
       (forward-line)
-      (save-excursion (replace-regexp " +" " " nil start end))
-      (pj-breaklines-and-justify (pj-get-line-tokens (pj-get-line))))))
+      ;; (save-excursion (replace-regexp " +" " " nil start end))
+      (pj-breaklines-and-justify (pj--get-line-tokens)))))
 
 
 (defun pj-unjustify-paragraph ()
